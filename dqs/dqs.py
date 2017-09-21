bdd222b72e01a4d7c7a4bb51147846a7817bfb35
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-09-21 17:21:12 -0500

    Should organize RL completely. need to fill in helper functions and integrate FC NN

--- dqs/dqs.py
+++ dqs/dqs.py
@@ -17,7 +17,7 @@ import random as rd
 # print("xi", xi)
 # print("resid", resid)
 
-def runDQS():
+def trainDQS(epochs=1, epsilon=.9):
   functionCount = 3
   maxOrder = 3
   maxElements = 3
@@ -25,18 +25,18 @@ def runDQS():
   henkelRows = 10
   dt = .001
 
-  epochs = 1
-  epsilon = .9
-
   for epoch in range(epochs):
     data, xiOracle = generateTrainingSample(functionCount, maxOrder, maxElements, coefficientMagnitude)
     V, dX, theta, norms = initializeSINDy(data[:, 0], henkelRows, functionCount, maxOrder, dt)
     xi, resid, rank, s = np.linalg.lstsq(theta, dX)
-    state = theta
-    # action space = np.zeros(theta.shape)
-    # actions.flatten()
-  
+    
+    # initialize RL vars
+    state = np.append(resid, xi)  # xi and residuals
+    action = np.zeros(state.shape) # selecting resid does nothing
     status = 1
+    cumulativeReward = 0
+    rewards = []
+
     while status:
 
       # get action estimates
@@ -44,40 +44,51 @@ def runDQS():
 
       # select action
       if rd.random > epsilon:
-        action, maxQ = randomAction(state)
+        action, currentQ = randomAction(state)
       else: 
-        action, maxQ = greedyAction(qVals)
+        action, currentQ = greedyAction(qVals)
       
       # take action
       newState = transition(state, action)
 
       # observe reward
       reward = rewardAt(newState, xiOracle)
+      cumulativeReward += reduce((lambda x, y: x + y), reward)
 
-      # get targetQ with look-ahead
+      # get target Q-values with look-ahead
       newQVals = predict(newState, batchSize=1)
-      newAction, lookforwardQ = greedyAction(newQVals)
-      targetQ, terminal = checkTarget(action, lookforwardQ, reward)
-      qUpdate = qVals
-      qUpdate[action] = targetQ
+      newAction, lookaheadQ = greedyAction(newQVals)
+      updates, terminal = getTarget(lookaheadQ, reward)
+      targetQ = qVals
+      targetQ[action, :] = updates
 
       # update model with target values
-      fitModel(state, qUpdate)
+      fitModel(state, targetQ)
 
-def checkTarget(action, newQ, reward):
+      # next steps
+      state = newState
+      status = terminal
+    rewards.append(cumulativeReward)
+  return rewards
+
+# get the target value given the lookahead and reward
+def getTarget(loohahead, reward):
+  terminalCount = 0
+  target = loohahead + reward
+  for i in range(len(current)):
+    if reward[i] == 0:
+      target[i] = 0
+      terminalCount += 1
   terminal = False
-  # if reward is >=0, newQ = reward
-  # otherwise add the two
-  # if all three >=0, terminal
-  return newQ, terminal
+  if terminalCount == 0:
+    terminal = True
+  return terminal, target
 
-# state has xi
+# change state, regress new state, return new state
 def transition(state, action):
   return state
 
-# actions : 
-# - one per vector
-# - can zero out, or set to one any row in vector
+# find the max Q val action in each column
 def greedyAction(qVals):
   action = []
   for col in qVals:
@@ -85,6 +96,7 @@ def greedyAction(qVals):
     action.append(new)
   return action
 
+# return a random action vector
 def randomAction(state):
   action = []
   for i in range(state):
@@ -95,16 +107,17 @@ def randomAction(state):
     # get Q's, zip before return
   return action
 
-# value net will be random number generator to begin with
-# return return Q vals for each column
-def predictState(state, batchSize):
+# return Q vals for each possible action
+  # take action, optimize, estimate value of new state
+def predict(state, batchSize):
   return
 
-# reward function : -1 if continuing, or +10 depending on if state is terminal
+# reward is 0 if oracle matched, -1 otherwise
 def rewardAt(state, oracle):
   return -1
 
-def fitModel(state, qUpdate, batchSize=1, nbEpoch=1, verbose=1):
+# backpropogation on Q function
+def fitModel(state, targetQ, batchSize=1, nbEpoch=1, verbose=1):
   return
 
 # experience replay array?
